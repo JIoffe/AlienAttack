1f8f4f1bc4ab8901739e9097d6d167f8bc979297
Jason Ioffe <Jason.Ioffe@insight.com>
2018-05-07 17:30:33 -0400

    Added stack

---
+++ js/utils/stack.js
@@ -0,0 +1,28 @@
+export class UIntStack{
+    constructor(capacity){
+        //OK, misleading... 
+        //but I'd rather return -1 on failure, even if we give up true UINT range
+        this.elements = new Int32Array(capacity);
+        this.n = 0;
+        this.size = capacity;
+    }
+
+    push(value){
+        if(this.n < this.size)
+            this.elements[this.n++] = value;
+    }
+
+    pop(){
+        if(this.n === 0)
+            return -1;
+
+        return this.elements[this.n-- - 1];
+    }
+
+    peek(){
+        if(this.n === 0)
+            return -1;
+
+        return this.elements[this.n - 1];
+    }
+}
